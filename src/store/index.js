6f4f8e3d25e17deb2cb60e15ece27d7f1e7748c4
Sun <276042491@qq.com>
2020-12-09 17:11:28 +0800

    hello saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,15 @@
 import { createStore, applyMiddleware } from "redux"
 
-import { composeWithDevTools } from "redux-devtools-extension"
+import { composeWithDevTools } from "redux-devtools-extension" // 调试工具
 
-import rootReducer from "./reducers"
+import rootReducer from "./reducers" // 根reducers
 
-export default createStore(rootReducer,composeWithDevTools())
+import createSagaMiddleware from "redux-saga" // 引入saga中间件
+
+let sagaMiddleware = createSagaMiddleware()
+
+import helloSaga  from "./sagas/Product"
+
+export default createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)))
+
+sagaMiddleware.run(helloSaga)
